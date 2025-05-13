d332c70de140270247f06a642ce69f91c1730b91
Abdulaziz1az <AbdululazizAbdirisak15@gmail.com>
2025-05-12 21:34:18 -0500

    Abdulaziz Abdirisak 5/12/25 This a Student class with three Attribute name, age, major, and method that displayInfo() that prints on info.

---
+++ OOP/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Abdulaziz";
+        s1.age = 22;
+        s1.major = "Computer Science";
+
+        s1.displayInfo();
+    }
+}
